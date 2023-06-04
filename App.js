a55c67590946e07342ef48445cd9dba460516f75
Kukreja <jayesh.kukreja@sap.com>
2023-06-04 13:04:14 +0530

    2nd Lecture foundation

--- App.js
+++ App.js
@@ -1,31 +1,23 @@
 import React  from "react";
 import ReactDOM from "react-dom/client";
 
-const heading = React.createElement (
-    "h1",
-    {
-        id: "title",
-    },
-    "Heading 1"
+const title = (
+    <h1 id="title" key="h1">
+        Namaste React
+    </h1>
 );
 
-const heading2 = React.createElement (
-    "h2",
-    {
-        id: "title",
-    },
-    "Heading 2"
-);
-
-const container = React.createElement (
-    "div",
-    {
-        id: "container",
-    },
-    [heading, heading2]
-)
-
-console.log(heading);
+const HeaderComponent = () => {
+    return (
+        <div>
+            {title}
+            <h2>
+                Hello1 
+            </h2>
+            <h2> how are </h2>
+        </div>
+    )
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(container);
+root.render(<HeaderComponent/>);
